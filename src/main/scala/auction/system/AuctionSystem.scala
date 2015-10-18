cc2c433db5191be1e635b0a5f4bd47cb53a08b35
novy154 <slnowak154@gmail.com>
2015-10-18 21:59:41 +0200

    Small fix

--- src/main/scala/auction/system/AuctionSystem.scala
+++ src/main/scala/auction/system/AuctionSystem.scala
@@ -23,20 +23,20 @@ class AuctionCoordinator extends Actor {
   }
 
   private def startSystem(): Unit = {
-    val auction1: ActorRef = context.actorOf(Auction.props(step = BigDecimal(0.50), initialPrice = BigDecimal(100)), "auction1")
+    val auction1: ActorRef = context.actorOf(Auction.props(step = BigDecimal(0.50), initialPrice = BigDecimal(12)), "auction1")
     val auction2: ActorRef = context.actorOf(Auction.props(step = BigDecimal(1), initialPrice = BigDecimal(0)), "auction2")
 
-    val buyer1: ActorRef = context.actorOf(Buyer.props(moneyToSpend = BigDecimal(500)), "buyer1")
-    val buyer2: ActorRef = context.actorOf(Buyer.props(moneyToSpend = BigDecimal(300)), "buyer2")
-    val buyer3: ActorRef = context.actorOf(Buyer.props(moneyToSpend = BigDecimal(600)), "buyer3")
+    val buyer1: ActorRef = context.actorOf(Buyer.props(moneyToSpend = BigDecimal(10)), "buyer1")
+    val buyer2: ActorRef = context.actorOf(Buyer.props(moneyToSpend = BigDecimal(20)), "buyer2")
+    val buyer3: ActorRef = context.actorOf(Buyer.props(moneyToSpend = BigDecimal(40)), "buyer3")
 
-    buyer1 ! StartBidding(amount = BigDecimal(5), auction = auction1)
-    buyer1 ! StartBidding(amount = BigDecimal(5), auction = auction2)
+    buyer1 ! StartBidding(amount = BigDecimal(0), auction = auction1)
+    buyer1 ! StartBidding(amount = BigDecimal(1), auction = auction2)
 
-    buyer2 ! StartBidding(amount = BigDecimal(60), auction = auction1)
-    buyer2 ! StartBidding(amount = BigDecimal(10), auction = auction2)
+    buyer2 ! StartBidding(amount = BigDecimal(1), auction = auction1)
+    buyer2 ! StartBidding(amount = BigDecimal(0), auction = auction2)
 
-    buyer3 ! StartBidding(amount = BigDecimal(66), auction = auction1)
+    buyer3 ! StartBidding(amount = BigDecimal(5), auction = auction1)
     buyer3 ! StartBidding(amount = BigDecimal(1), auction = auction2)
   }
 }

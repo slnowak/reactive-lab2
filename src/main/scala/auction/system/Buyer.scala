0c9075cf4e70bb361fd872e91d9842bba4c684c2
novy154 <slnowak154@gmail.com>
2015-10-18 19:38:06 +0200

    Added starting case to buyer

--- src/main/scala/auction/system/Buyer.scala
+++ src/main/scala/auction/system/Buyer.scala
@@ -3,7 +3,7 @@ package auction.system
 import akka.actor.{Actor, ActorRef}
 import akka.event.LoggingReceive
 import auction.system.Auction.{AuctionWon, BidAccepted, BidTooLow, BidTopBySomeoneElse}
-import auction.system.Buyer.{Bid, SentBid, WonAuction}
+import auction.system.Buyer.{Bid, SentBid, StartBidding, WonAuction}
 
 /**
  * Created by novy on 18.10.15.
@@ -13,35 +13,38 @@ class Buyer(var moneyToSpend: BigDecimal) extends Actor {
   private var wonAuctions: Set[WonAuction] = Set()
   private var offers: Set[SentBid] = Set()
 
-
   override def receive: Receive = LoggingReceive {
+    case StartBidding(initialBid, auction) => sendBidIfCanAfford(initialBid, auction)
     case BidAccepted => _
     case BidTooLow(offered, expected) => tryToTopBid(offered, expected, sender())
-    case AuctionWon(winningOffer) => handleAuctionWon(winningOffer, sender())
     case BidTopBySomeoneElse(previous, actualHighest, step) => handleBidTopBySomeoneElse(previous, actualHighest, step, sender())
+    case AuctionWon(winningOffer) => handleAuctionWon(winningOffer, sender())
   }
 
-
-  def tryToTopBid(previousOffer: BigDecimal, expectedOffer: BigDecimal, auction: ActorRef): Unit = {
+  private def tryToTopBid(previousOffer: BigDecimal, expectedOffer: BigDecimal, auction: ActorRef): Unit = {
     offers = offers - SentBid(previousOffer, auction)
 
-    if (canAfford(expectedOffer)) {
-      offers = offers + SentBid(expectedOffer, auction)
-      auction ! Bid(expectedOffer)
+    sendBidIfCanAfford(expectedOffer, auction)
+  }
+
+  private def sendBidIfCanAfford(offer: BigDecimal, auction: ActorRef): Unit = {
+    if (canAfford(offer)) {
+      offers = offers + SentBid(offer, auction)
+      auction ! Bid(offer)
     }
   }
 
-  def handleAuctionWon(winningOffer: BigDecimal, auction: ActorRef): Unit = {
+  private def handleAuctionWon(winningOffer: BigDecimal, auction: ActorRef): Unit = {
     offers = offers - SentBid(winningOffer, auction)
     moneyToSpend -= winningOffer
     wonAuctions = wonAuctions + WonAuction(winningOffer, auction)
   }
 
-  def handleBidTopBySomeoneElse(previous: BigDecimal, actualHighest: BigDecimal, requiredStep: BigDecimal, auction: ActorRef): Unit = {
+  private def handleBidTopBySomeoneElse(previous: BigDecimal, actualHighest: BigDecimal, requiredStep: BigDecimal, auction: ActorRef): Unit = {
     tryToTopBid(previous, actualHighest + requiredStep, auction)
   }
-  
-  def canAfford(offer: BigDecimal): Boolean = {
+
+  private def canAfford(offer: BigDecimal): Boolean = {
     val totalOffers: BigDecimal = offers
       .map(_.amount)
       .foldLeft(BigDecimal(0))(_ + _)
@@ -52,11 +55,12 @@ class Buyer(var moneyToSpend: BigDecimal) extends Actor {
 
 object Buyer {
 
-  case class SentBid(amount: BigDecimal, action: ActorRef)
+  case class StartBidding(amount: BigDecimal, auction: ActorRef)
 
-  case class Bid(amount: BigDecimal) {
-    require(amount > 0)
-  }
+  case class SentBid(amount: BigDecimal, auction: ActorRef)
+
+  case class Bid(amount: BigDecimal)
 
   case class WonAuction(amount: BigDecimal, auction: ActorRef)
+
 }
